766e89b6f6969db833a5bba502ae666efe9cc583
notBigO <2003varunp@gmail.com>
2024-07-23 21:57:47 +0530

    chore: Add pnet and sysinfo dependencies to Cargo.toml and implement network monitoring functionality in main.rs and lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
+use sniffme::network::NetworkMonitor;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <interface_name>", args[0]);
+        std::process::exit(1);
+    }
+
+    let interface_name = &args[1];
+
+    println!("Starting network monitor on interface: {}", interface_name);
+
+    let mut monitor = NetworkMonitor::new(interface_name);
+    monitor.start();
 }
